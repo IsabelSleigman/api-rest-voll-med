c8200e2fbe66ec08dc0255fc0544cb2f120f5d1e
isabel.sleigman <sleimusic@gmail.com>
2022-11-21 21:39:59 -0300

    paginação em rota get paciente

--- src/main/java/med/voll/apirestvollmed/controller/PacienteController.java
+++ src/main/java/med/voll/apirestvollmed/controller/PacienteController.java
@@ -3,13 +3,14 @@ package med.voll.apirestvollmed.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.apirestvollmed.paciente.DadosCadastroPaciente;
+import med.voll.apirestvollmed.paciente.DadosListagemPaciente;
 import med.voll.apirestvollmed.paciente.Paciente;
 import med.voll.apirestvollmed.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -22,5 +23,9 @@ public class PacienteController {
     public String cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
         return "Cadastrado com sucesso!";
+    }
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao ){
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
--- src/main/java/med/voll/apirestvollmed/paciente/DadosListagemPaciente.java
+++ src/main/java/med/voll/apirestvollmed/paciente/DadosListagemPaciente.java
@@ -1,2 +1,7 @@
-package med.voll.apirestvollmed.paciente;public record DadosListagemPaciente() {
+package med.voll.apirestvollmed.paciente;
+
+public record DadosListagemPaciente(String nome, String email, String cpf) {
+    public DadosListagemPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+    }
 }
