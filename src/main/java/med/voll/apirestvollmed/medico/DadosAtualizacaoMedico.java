13e5ae32ba9d8b09ff199ebb73a5bb859fb40c59
isabel.sleigman <sleimusic@gmail.com>
2022-11-23 21:13:34 -0300

    metodos de atualizacao de medico

--- src/main/java/med/voll/apirestvollmed/medico/DadosAtualizacaoMedico.java
+++ src/main/java/med/voll/apirestvollmed/medico/DadosAtualizacaoMedico.java
@@ -1,2 +1,12 @@
-package med.voll.apirestvollmed.medico;public record DadosAtualizacaoMedico() {
+package med.voll.apirestvollmed.medico;
+
+import jakarta.validation.constraints.NotNull;
+import med.voll.apirestvollmed.endereco.DadosEndereco;
+
+public record DadosAtualizacaoMedico(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco) {
 }
