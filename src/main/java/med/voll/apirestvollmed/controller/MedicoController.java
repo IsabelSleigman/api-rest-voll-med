24cdcd16b40176ed8aba1cd556d24796adf87342
isabel.sleigman <sleimusic@gmail.com>
2022-11-23 21:13:22 -0300

    nova rota de atualizacao de medico

--- src/main/java/med/voll/apirestvollmed/controller/MedicoController.java
+++ src/main/java/med/voll/apirestvollmed/controller/MedicoController.java
@@ -1,10 +1,7 @@
 package med.voll.apirestvollmed.controller;
 
 import jakarta.validation.Valid;
-import med.voll.apirestvollmed.medico.DadosCadastroMedico;
-import med.voll.apirestvollmed.medico.DadosListagemMedico;
-import med.voll.apirestvollmed.medico.Medico;
-import med.voll.apirestvollmed.medico.MedicoRepository;
+import med.voll.apirestvollmed.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -31,5 +28,14 @@ public class MedicoController {
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+
     }
 }
